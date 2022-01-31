c6a126537a9a5b0a912614d1a5625ce89cf1139b
Rafe <rafejm@hotmail.com>
2022-01-31 12:58:14 -0500

    'upload'

---
+++ src/App.svelte
@@ -0,0 +1,98 @@
+<script>
+</script>
+
+<head>
+	<script src="https://kit.fontawesome.com/eb06444889.js" crossorigin="anonymous"></script>
+</head>
+<main>
+	<div class="appInformation">
+		<img class="appIcon" alt="logo" src="/build/assets/logo_rounded.png">
+		<div class="appName">
+			<h1>TumYum</h1>
+		</div>
+		<div class="appDescription">
+			<p>tap into your local food scene</p>
+		</div>
+		<div class="interestForm">
+			<a class="typeform" href="https://za1e6wwbj28.typeform.com/to/yy4moyuj"><img class ="betaTestImg" alt="betaSignUp" src="/build/assets/beta-sign-up.png"></a>
+		</div>
+	</div>
+	<div class="footer">
+		<section class="social">
+			<div class="socialIcons">
+				<a href="https://www.instagram.com/tumyumapp/"><i class="fab fa-instagram fa-3x"></i></a>
+				<a href="https://discord.gg/QUfFAX9X"><i class="fab fa-discord fa-3x"></i></a>
+				<a href="https://www.youtube.com/channel/UCm0tWiLs05bMl0Lv8bkj-bw"><i class="fab fa-youtube fa-3x"></i></a>
+			</div>
+			<div class="privacyPolicy">
+				<a href="https://www.termsfeed.com/live/cd5b92d8-3c69-400f-a9ac-be8020b4be6a">Privacy</a>
+			</div>
+		</section>
+	</div>
+</main>
+
+<style>
+
+	.appInformation {
+		display: flex;
+		flex-direction: column; 
+		justify-content: center;
+		align-items: center; 
+		padding-top: 110px;
+		flex-wrap: wrap; 
+		background-image: url('assets/background2.jpg');
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+	}
+
+	.appIcon {
+		width: 120px;
+		height: 120px;
+	}
+
+	.appName {
+		font-family: 'ubie';
+		text-align: center;
+		color: #C2EB99;
+		font-size: 3rem; 
+	}
+
+	.appDescription {
+		font-family: 'Playfull'; 
+		text-align: center;
+		color: #db10ca;
+		font-size: 2rem; 
+	}
+
+	.interestForm {
+		display: flex;
+		margin-top: 42px;
+		height: 60px;
+	}
+
+	.footer {
+		padding-top: 100px;
+		display: flex; 
+		flex-wrap: wrap;
+		flex-direction: row;
+		justify-content: center; 
+		align-content: flex-end;
+	}
+
+	.privacyPolicy {
+		text-align: center; 
+	}
+
+@font-face {
+    font-family: 'ubie';
+    src: url('/build/assets/ubie-webfont.woff') format('woff');
+	font-weight: 1000;
+}	
+
+@font-face {
+	font-family: 'Playfull';
+	src: url('/build/assets/Playfull.ttf') format('ttf');
+}
+
+</style>
